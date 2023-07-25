14ed2764a7e3298118117b7f838fb35f2e84f056
alexandre-damiens <94200143+alexandre-damiens@users.noreply.github.com>
2023-07-25 16:06:35 +0200

    Delete script directory

--- script/envoyer.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-$(".send-btn").click(function(){
-$(".contact-form").attr('action','mailto:alex.dams76@gmail.com?subject='+$('#sjt').val()+'&body='+$("#message").val());
-/*après remplissage du formulaire la totalité des éléments seront utilisés pour l'envoie d'un mail*/
-});
-});
